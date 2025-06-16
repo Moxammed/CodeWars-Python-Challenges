228dc794396c878b26769084cdf954b9b7660b04
Macos <mohammed.kamal@tiu.edu.iq>
2025-06-16 13:39:00 -0400

    14- Sum Arrays

---
+++ 12- Even or Odd .py
@@ -0,0 +1,21 @@
+'''
+------Instructions------
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+------Test Cases------'
+  even_or_odd(1), "Odd"
+  even_or_odd(2) should return "Even"'
+  even_or_odd(-1) should return "Odd"'  
+  even_or_odd(-2) should return "Even"'
+  even_or_odd(0) should return "Even"'
+ 
+
+------Siolution------
+'''
+
+
+def even_or_odd(number):
+    return "Even" if number%2==0 else "Odd"
+
---
+++ 13- Generate user links .py
@@ -0,0 +1,31 @@
+'''
+------Instructions------
+
+Generate user links
+Your task is to create userlinks for the url, you will be given a username and must return a valid link.
+
+Example
+generate_link('matt c')
+http://www.codewars.com/users/matt%20c
+reference
+use this as a reference https://www.w3schools.com/tags/ref_urlencode.asp
+
+
+
+
+------Test Cases------'
+  'matt c', 'http://www.codewars.com/users/matt%20c'
+  'g964', 'http://www.codewars.com/users/g964'
+  'GiacomoSorbi', 'http://www.codewars.com/users/GiacomoSorbi'
+  'ZozoFouchtra', 'http://www.codewars.com/users/ZozoFouchtra'
+  'colbydauph', 'http://www.codewars.com/users/colbydauph'
+
+  
+------Siolution------
+'''
+import urllib.parse
+
+def generate_link(user):
+    return f"http://www.codewars.com/users/{urllib.parse.quote(user)}"
+
+
