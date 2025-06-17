26bec29ef8052529ee2888ddc5e1d7e00e9d2b98
Macos <mohammed.kamal@tiu.edu.iq>
2025-06-16 21:44:56 -0400

    15- Remove exclamation marks

---
+++ 15- Remove exclamation marks.py
@@ -0,0 +1,7 @@
+#Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+#("Hello World!"), "Hello World")
+
+
+def remove_exclamation_marks(s):
+    return s.replace("!", "")
